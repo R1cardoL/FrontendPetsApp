291ed951dc8ec3eae5e69b1ae7235490a2bb82e0
LorenaC02 <lorenacampuzano083@gmail.com>
2022-11-22 11:04:57 -0500

    Se agrega el crud para mascota

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarMascotaComponent } from './mascota/buscar-mascota/buscar-mascota.component';
 import { CrearMascotaComponent } from './mascota/crear-mascota/crear-mascota.component';
 import { EditarMascotaComponent } from './mascota/editar-mascota/editar-mascota.component';
+import { EliminarMascotaComponent } from './mascota/eliminar-mascota/eliminar-mascota.component';
 import { BuscarProductoServicioComponent } from './producto-servicio/buscar-producto-servicio/buscar-producto-servicio.component';
 import { CrearProductoServicioComponent } from './producto-servicio/crear-producto-servicio/crear-producto-servicio.component';
 import { EditarProductoServicioComponent } from './producto-servicio/editar-producto-servicio/editar-producto-servicio.component';
@@ -30,12 +32,20 @@ const routes: Routes = [
     component: EditarProductoServicioComponent
   },
   {
+    path: "listar-mascotas",
+    component: BuscarMascotaComponent
+  },
+  {
     path: 'crear-mascota',
     component: CrearMascotaComponent
   },
   {
-    path: 'editar-mascota',
+    path: 'editar-mascota/:id',
     component: EditarMascotaComponent
+  },
+  {
+    path: 'eliminar-mascota/:id',
+    component: EliminarMascotaComponent
   }
 ];
 
